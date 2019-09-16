ba573401f443c4a5fc72d1e907fb18ec423be631
IvoMoor <ivo.moor@just-bi.nl>
2019-09-16 15:12:56 +0200

    Update version

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route } from 'react-router-dom';
-import FlowPage from './Flow';
-import MindPage from './Mind';
-import KoniPage from './Koni';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/app';
+import './index.css';
 
 ReactDOM.render(
-  <Router>
-    <Route path="/flow" component={FlowPage} />
-    <Route path="/mind" component={MindPage} />
-    <Route path="/koni" component={KoniPage} />
-  </Router>,
-  document.getElementById('root'),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
 );
